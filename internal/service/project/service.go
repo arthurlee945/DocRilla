451de209fbf4420aad56fa4c0370dd22ae09b783
Arthur Lee <arthur.lee945@gmail.com>
2024-04-20 11:21:03 -0500

    starting on Project Service

--- internal/service/project/service.go
+++ internal/service/project/service.go
@@ -1,11 +1,28 @@
 package project
 
-type Service struct {
+import (
+	"context"
+
+	"github.com/arthurlee945/Docrilla/internal/model"
+)
+
+type Service interface {
+}
+
+type Project struct {
+	model.Project
+}
+
+type service struct {
 	repository Repository
 }
 
-func NewService(r Repository) *Service {
-	return &Service{
+func NewService(r Repository) Service {
+	return &service{
 		repository: r,
 	}
+}
+
+func (s *service) GetAll(ctx context.Context, opts struct{ limit string }) (*Project, error) {
+	return nil, nil
 }
