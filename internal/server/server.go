868d0f76f7972b620f13973ec6a0e6160557c273
Arthur Lee <arthur.lee945@gmail.com>
2024-04-29 22:19:08 -0500

    sync

--- internal/server/server.go
+++ internal/server/server.go
@@ -2,11 +2,14 @@ package server
 
 import (
 	"net/http"
+
+	"github.com/arthurlee945/Docrilla/internal/logger"
 )
 
 // https://grafana.com/blog/2024/02/09/how-i-write-http-services-in-go-after-13-years/
-func New() http.Handler {
+func New(logger *logger.Logger) http.Handler {
 	mux := http.NewServeMux()
+
 	// ctx := context.Background()
 	// publicRouter := http.NewServeMux()
 	// protectedRouter := http.NewServeMux()
