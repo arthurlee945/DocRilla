4d73a817d566fab8452feaf68bf2bb37a49105ad
Arthur Lee <arthur.lee945@gmail.com>
2024-04-30 21:07:36 -0500

    working on field repo

--- internal/service/field/repository.go
+++ internal/service/field/repository.go
@@ -14,6 +14,17 @@ type repository struct {
 	db *sqlx.DB
 }
 
-func Get(ctx context.Context, uuid string) (*model.Field, error) {
+func NewRepository(db *sqlx.DB) Repository {
+	return &repository{
+		db,
+	}
+}
+
+func (r *repository) Get(ctx context.Context, uuid string) (*model.Field, error) {
+	field := &model.Field{}
+	if err := r.db.GetContext(ctx, field, `SELECT * FROM field WHERE uuid = $1`, uuid); err != nil {
+		return nil, err
+	}
+
 	return nil, nil
 }
