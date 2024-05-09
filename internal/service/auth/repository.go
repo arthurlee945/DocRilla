017224141ced546c51ee5d3208fb83d8a79e0d52
Arthur Lee <arthur.lee945@gmail.com>
2024-05-08 21:32:34 -0500

    adding-base-temp

---
+++ internal/service/auth/repository.go
@@ -0,0 +1,12 @@
+package auth
+
+import "github.com/jmoiron/sqlx"
+
+type Repository interface{}
+type repository struct {
+	db *sqlx.DB
+}
+
+func NewRepository(db *sqlx.DB) Repository {
+	return &repository{db}
+}
